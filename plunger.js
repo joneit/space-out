085cd995054d84edbf3236aba09de86cb34c51c1
Jonathan Eiten <jonathan@openfin.co>
2017-06-08 20:14:27 -0400

    not sure what this is

--- plunger.js
+++ plunger.js
@@ -1,6 +1,6 @@
 window.onload = function() {
-    var BLACK = 0, WHITE = 0xffffff, GOLD = 0xc8c876, BLUE = 0x761ec8;
-    var PI_OVER_2 = Math.PI / 2, PI_OVER_4 = Math.PI / 4, PI_OVER_8 = Math.PI / 8;
+    var BLACK = 0, WHITE = 0xffffff, GOLD = 0xd8c876, BLUE = 0x761ec8;
+    var PI_TIMES_2 = Math.PI * 2, PI_OVER_2 = Math.PI / 2, PI_OVER_4 = Math.PI / 4, PI_OVER_8 = Math.PI / 8;
 
     var width = window.innerWidth;
     var height = window.innerHeight;
@@ -62,8 +62,8 @@ window.onload = function() {
 
     function getCamera() {
         var camera = new THREE.PerspectiveCamera(45, width / height, 0.1, 10000);
-        camera.position.y = 80;
-        camera.position.z = 50;
+        camera.position.y = 150;
+        camera.position.z = 150;
         camera.lookAt(group.position);
         return camera;
     }
@@ -93,56 +93,43 @@ window.onload = function() {
 
     function getGroup() {
         var group = new THREE.Group();
+        var columnBSP = getFlutedTubeBSP(12, 96, 15);
 
-        material = new THREE.MeshLambertMaterial({ color: BLUE });
+        material = new THREE.MeshLambertMaterial({ color: GOLD });
 
-        group.add(getPlunger(5, 0, 0, 0));
-        // for (var x = -9; x <= 9; ++x)
-        //     for (var z = -19; z <= 19; ++z)
-        //         group.add(getFlopper(15 * x, 0, 10 * z));
+        for (x = -3; x <= 3; ++x) {
+            group.add(getPlunger(x * 36, 24, 0));
+        }
 
         return group;
 
-        function getPlunger(n, x, y, z) {
-            var group = new THREE.Group();
-            var plunger = Array(n);
-            for (var i = n; i--;) {
-                group.add(plunger[i] = getFlutedTube(3 + i, 32, 8 + 2 * i, 0, 8, 0));
-                break;
-            }
-            plungers.push(plunger);
-            group.position.x = x;
-            group.position.y = y;
-            group.position.z = z;
-            // group.rotation.z = 1;
-            return group;
+        function getPlunger(x, y, z) {
+            var mesh = columnBSP.toMesh(material);
+            mesh.applyMatrix(new THREE.Matrix4().makeTranslation(x, y, z));
+            return mesh;
         }
 
-        function getTube(radius, height, radiusSegments, dx, dy, dz) {
-            var geometry = new THREE.CylinderGeometry(radius, radius, height, radiusSegments);
+        function getTube(r, height, radiusSegments, dx, dy, dz) {
+            var geometry = new THREE.CylinderGeometry(r, r, height, radiusSegments);
             geometry.applyMatrix(new THREE.Matrix4().makeTranslation(dx, dy, dz));
             return new THREE.Mesh(geometry, material);
         }
 
-        function getFlutedTube(radius, height, flutes, dx, dy, dz) {
-            var geometry = new THREE.CylinderGeometry(radius, radius, height, 15);
+        function getFlutedTubeBSP(r, height, flutes, dx, dy, dz) {
+            var geometry = new THREE.CylinderGeometry(r, r, height, 15);
             var mesh = new THREE.Mesh(geometry);
             var BSP = new ThreeBSP(mesh);
 
-            var flute = new THREE.CylinderGeometry(.15 * radius, .15 * radius, height, 25);
-            var r = 1.03 * radius;
-            for (var th = 0; th < 2 * Math.PI; th += Math.PI / 7.5) {
+            var flute = new THREE.CylinderGeometry(.15 * r, .15 * r, height, 25);
+            r *= 1.03;
+            for (var th = 0; th < 2 * Math.PI; th += PI_TIMES_2 / flutes) {
                 var x = r * Math.cos(th), z = r * Math.sin(th);
                 flute.applyMatrix(new THREE.Matrix4().makeTranslation(x, 0, z));
                 BSP = BSP.subtract(new ThreeBSP(new THREE.Mesh(flute)));
                 flute.applyMatrix(new THREE.Matrix4().makeTranslation(-x, 0, -z));
             }
 
-            mesh = BSP.toMesh(material);
-
-            mesh.applyMatrix(new THREE.Matrix4().makeTranslation(dx, dy, dz));
-            
-            return mesh;
+            return BSP;
         }
     }
 };
